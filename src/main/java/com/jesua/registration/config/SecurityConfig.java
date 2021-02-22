10e7799a9e938561429ccf4ef7c8f0aa4475dd39
Stanislav Haluska <stanislav.haluska@gmail.com>
2021-02-22 20:52:14 +0100

    add missing props

---
+++ src/main/java/com/jesua/registration/config/SecurityConfig.java
@@ -0,0 +1,92 @@
+package com.jesua.registration.config;
+
+import com.jesua.registration.security.exception.CustomUnauthorizedHandler;
+import com.jesua.registration.security.exception.RestAccessDeniedHandler;
+import com.jesua.registration.security.filters.JwtRequestFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.header.writers.StaticHeadersWriter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${origin.url}")
+    private String originUrl;
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+    private final JwtRequestFilter jwtRequestFilter;
+    private final CustomUnauthorizedHandler unathorizedHandler;
+    private final RestAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList(originUrl));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .headers()
+                .contentTypeOptions()
+                .and().xssProtection()
+                .and().cacheControl()
+                .and().httpStrictTransportSecurity()
+                .and().frameOptions()
+                .and().httpStrictTransportSecurity()
+                .and().referrerPolicy()
+                .and().addHeaderWriter(new StaticHeadersWriter("X-Content-Security-Policy", "script-src 'self'"))
+                .and().cors()
+                .and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unathorizedHandler)
+                .accessDeniedHandler(accessDeniedHandler)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/password/userAccount", "/password/resetPassword", "/password/changePassword", "/users/signin", "/events/activeEventList", "/registration/allFollowersByActiveEvents", "/registration/add", "/registration/unsubscribe/**").permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
