0c46d0c41c004b675ad0379c905207d8f3799a0a
stanislav.haluska@gmail.com <stanislav.haluska@gmail.com>
2021-07-22 12:45:12 +0200

    add unique constraint

--- src/main/java/com/jesua/registration/entity/Project.java
+++ src/main/java/com/jesua/registration/entity/Project.java
@@ -4,18 +4,25 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @Entity
+@Table (uniqueConstraints = {
+        @UniqueConstraint(name = "uk_project__short_name", columnNames = "short_name")
+})
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
 public class Project extends BasePrivateEntity {
 
     @NotNull
     @Size(max = 100)
+    @Column(name = "short_name", unique=true)
     private String shortName;
 
     @NotNull
